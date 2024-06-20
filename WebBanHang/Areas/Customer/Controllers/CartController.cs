e2591458079e242e60281dcd7bcd6ec805541d89
ADMIN <ADMIN@PC223>
2024-06-20 11:26:05 +0700

    Add project files.

---
+++ WebBanHang/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebBanHang.Models;
+using WebBanHang.Helpers;
+namespace WebBanHang.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        //hiển thị giỏ hàng
+
+        public IActionResult Index()
+        {
+            Cart cart = HttpContext.Session.GetJson<Cart>("CART");
+            if (cart == null)
+            {
+                cart = new Cart();
+            }
+            return View(cart);
+        }
+        //xử lý thêm sản phẩm vào giỏ
+        public IActionResult AddToCart(int productId)
+        {
+            var product = _db.Products.FirstOrDefault(x => x.Id == productId);
+            if (product != null)
+            {
+                Cart cart = HttpContext.Session.GetJson<Cart>("CART");
+                if (cart == null)
+                {
+                    cart = new Cart();
+                }
+                cart.Add(product, 1);
+                HttpContext.Session.SetJson("CART", cart);
+                //return Json(new { msg = "Product added to cart", qty = cart.Quantity });
+                return RedirectToAction("Index");
+            }
+            return Json(new { msg = "error" });
+        }
+        public IActionResult GetQuantityOfCart()
+        {
+            Cart cart = HttpContext.Session.GetJson<Cart>("CART");
+            if (cart != null)
+            {
+                return Json(new { qty = cart.Quantity });
+            }
+            return Json(new { qty = 0 });
+        }
+        public IActionResult UpdateToCart(int productId, int qty)
+        {
+            var product = _db.Products.FirstOrDefault(x => x.Id == productId);
+            if (product != null)
+            {
+                Cart cart = HttpContext.Session.GetJson<Cart>("CART");
+                if (cart == null)
+                {
+                    cart = new Cart();
+                }
+                cart.Update(productId, qty);
+                HttpContext.Session.SetJson("CART", cart);
+                // return Json(new { msg="success", qty = cart.Quantity});
+                return RedirectToAction("Index");
+            }
+            return Json(new { msg = "error" });
+        }
+        public IActionResult Delete(int productId)
+        {
+            var product = _db.Products.FirstOrDefault(x => x.Id == productId);
+            if (product != null)
+            {
+                Cart cart = HttpContext.Session.GetJson<Cart>("CART");
+                if (cart == null)
+                {
+                    cart = new Cart();
+                }
+                cart.Remove(productId);
+                HttpContext.Session.SetJson("CART", cart);
+                // return Json(new { msg="success", qty = cart.Quantity});
+                return RedirectToAction("Index");
+            }
+            return Json(new { msg = "error" });
+        }
+        #region API
+        public IActionResult AddToCartAPI(int productId)
+        {
+            var product = _db.Products.FirstOrDefault(x => x.Id == productId);
+            if (product != null)
+            {
+                Cart cart = HttpContext.Session.GetJson<Cart>("CART");
+                if (cart == null)
+                {
+                    cart = new Cart();
+                }
+                cart.Add(product, 1);
+                HttpContext.Session.SetJson("CART", cart);
+                return Json(new { msg = "Product added to cart", qty = cart.Quantity });
+            }
+            return Json(new { msg = "error" });
+        }
+        #endregion
+    }
+}
